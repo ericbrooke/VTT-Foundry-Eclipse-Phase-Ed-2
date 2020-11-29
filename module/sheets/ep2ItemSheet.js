80da8ca21b9df5215c41b0fdf6ca7ccbcf5ab6db
Eric Brooke <ericbrooke@mac.com>
2020-11-29 11:42:58 -0600

    Editor Bug fixed, CSS adjusted

--- module/sheets/ep2ItemSheet.js
+++ module/sheets/ep2ItemSheet.js
@@ -3,12 +3,12 @@ export default class ep2ItemSheet extends ItemSheet {
 
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
-      width: 530,
+      width: 630,
       height: 340,
       classes: ["ep2", "sheet", "item"]
     });
   }
-  
+
   get template(){
     return `systems/ep2/templates/sheets/${this.item.data.type}-sheet.html`;
   }
