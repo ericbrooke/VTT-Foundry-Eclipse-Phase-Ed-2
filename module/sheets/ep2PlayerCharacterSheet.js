aee8e0b30e90de7483d400cf41ad836bc5c4cd5e
Eric Brooke <ericbrooke@mac.com>
2021-01-05 12:23:39 -0600

    Character sheet now displays, added partials

--- module/sheets/ep2PlayerCharacterSheet.js
+++ module/sheets/ep2PlayerCharacterSheet.js
@@ -7,3 +7,15 @@ export default class ep2PlayerCharacterSheet extends ActorSheet {
     });
   }
 }
+
+get template(){
+  return `systems/ep2/templates/sheets/${this.item.data.type}-sheet.hbs`;
+}
+
+getData() {
+  const data = super.getData();
+
+  data.config = CONFIG.ep2;
+
+  return data;
+}
